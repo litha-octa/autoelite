10de661062b3ef3c1c4b8f7716d1fb81e12a89d5
litha-octa <lithaoctadelistia@gmail.com>
2026-04-16 11:01:02 +0700

    delete unused component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers/Providers";
-import TopBar from "./components/layout/TopBar";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
